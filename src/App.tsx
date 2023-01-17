947c0bf6008f7ffcf23f841993904dfcb1ff25a1
AKKK1 <akakichachava1@gmail.com>
2023-01-17 09:35:06 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Calculator from './components/Calculator'
+import styled from 'styled-components'
+
+function App() {
+
+  return (
+    <div className="App">
+    <Calculator/>
+    </div>
+  )
+}
+
+export default App
